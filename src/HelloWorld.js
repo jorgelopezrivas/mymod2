a24866a8613cfa1d1c6067fac65ed86e908559ea
Jorge L. Rivas <jorge.lopez@karalundi.com.mx>
2019-07-01 09:12:16 -0500

    test

---
+++ src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+class HelloWorld extends Component {
+    render() {
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        )
+    }
+}
+
+export { HelloWorld} ;
